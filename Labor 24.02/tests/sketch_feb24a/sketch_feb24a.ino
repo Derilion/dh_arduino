c06b60950a7a4954d6e9b390beb7dbc565c8363f
max <mbier@gmx.net>
2016-02-24 14:48:18 +0100

    version 1 all sensors merged

--- Labor 24.02/tests/sketch_feb24a/sketch_feb24a.ino
+++ Labor 24.02/tests/sketch_feb24a/sketch_feb24a.ino
@@ -3,15 +3,42 @@
  * 
  * Date: 24.04.16
  * 
- * Desc: All 
+ * Desc: All Sensor Readings merged in one File
  */
 
+#define distPin A6
+
+/*
+ * Init 
+ */
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(distPin, INPUT);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop () 
+{
+  range = distanceSensor_test();
+}
 
+
+/*
+ * functions for sharp distance sensor
+ */
+
+uint16_t distanceSensor_test()
+{
+  uint16_t value = analogRead (pin);
+  uint16_t range //= get_gp2d12 (value);
+  if (value < 10) range = 10;
+  else range = value;
+  range  = ((67870.0 / (range - 3.0)) - 40.0)
+// unneccesary range returns for serial monitor
+//  Serial.println (value);
+//  Serial.print (range);
+//  Serial.println (" mm");
+//  Serial.println ();
+  delay (500);
+  return range;
 }
