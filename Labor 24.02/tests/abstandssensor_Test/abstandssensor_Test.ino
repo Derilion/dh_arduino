7b4c101eaf0f43a32d9ae212bddfccea389e5d9b
JS5342 <js5342@users.noreply.github.com>
2016-02-24 13:26:54 +0100

    distance sensor test

---
+++ Labor 24.02/tests/abstandssensor_Test/abstandssensor_Test.ino
@@ -0,0 +1,34 @@
+/******** start code ********/
+/*
+ *  created   2013-07-12
+ *  by    lisper ([email protected])
+ *  function  test gp2d12, read value from A0
+ *      
+ *      VCC -- VCC  
+ *      GND -- GND  
+ *      Signal -- Analog 0 
+ */
+
+#define pin A6
+
+void setup () {
+  Serial.begin (9600);
+  pinMode (pin, INPUT);
+}
+
+void loop () {
+  uint16_t value = analogRead (pin);
+  uint16_t range = get_gp2d12 (value);
+  Serial.println (value);
+  Serial.print (range);
+  Serial.println (" mm");
+  Serial.println ();
+  delay (500);
+}
+
+uint16_t get_gp2d12 (uint16_t value) {
+  if (value < 10) value = 10;
+  return ((67870.0 / (value - 3.0)) - 40.0);
+}
+
+/******** end code ********/
